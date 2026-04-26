3f1c14654de746db35c1bb11e5919b518776a7e0
omeroastore-ops <omero.astore@gmail.com>
2026-04-26 14:30:33 +0200

    Delete components directory

--- components/PlacesMap.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Place } from "@/types/models";
-
-export interface PlacesMapProps {
-  places: Place[];
-  selectedId?: string | null;
-  onMarkerPress?: (place: Place) => void;
-}
-
-export function PlacesMap(props: PlacesMapProps): JSX.Element;
